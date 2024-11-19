a153c03ba1d0e65140f10f3531eb8bbf1861c934
Jack <jack@163.com>
2024-11-19 17:33:18 +0800

    优化描述

--- worker.js
+++ worker.js
@@ -4,11 +4,11 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '266d4558-4d2a-4c67-a5c3-91cf0dc7e9e2';
+let userID = 'b45a34fb-7b65-46cc-a976-0e4d127fedda';
 
-let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org'
+let proxyIP = '';// 'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org'
 
-let sub = '';// 避免项目被滥用，现已取消内置订阅器
+let sub = '';// 现已取消内置订阅器
 let subconverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假uuid和host订阅。
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
 let subProtocol = 'https';
